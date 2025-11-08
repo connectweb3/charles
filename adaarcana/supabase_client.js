a5acb18cd9c1db1ea5a4b43eb4e59f3a8d4abf96
Junkora <fitmac_bunso24@yahoo.com>
2025-11-08 13:58:17 +0800

    1

--- adaarcana/supabase_client.js
+++ adaarcana/supabase_client.js
@@ -26,8 +26,7 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   },
   global: {
     headers: {
-      apikey: SUPABASE_ANON_KEY,
-      Authorization: `Bearer ${SUPABASE_ANON_KEY}`
+      apikey: SUPABASE_ANON_KEY
     }
   }
 });
